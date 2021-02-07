18b368d7e62eb365df4683d546ab9b4b8416b0fc
ljs <lenon461@gmail.com>
2021-02-07 16:44:25 +0900

    trade styling add

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,4 +1,4 @@
-import { BootstrapVue } from "bootstrap-vue";
+import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import "bootstrap/dist/css/bootstrap.css";
 import Vue from "vue";
@@ -9,6 +9,7 @@ import store from "./store";
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
 
 new Vue({
   router,
--- front/src/views/Trade.vue
+++ front/src/views/Trade.vue
@@ -1,10 +1,62 @@
 <template>
   <div class="trade">
     <SymbolSwitcher />
-    <div class="order-wrap">
+    <div class="order-container">
       <!-- <OrderBox class="order-box" /> -->
-      <div class="order-box">
-        <b-card no-body>
+      <div class="order-box-container">
+        <div class="order-box-wrap">
+          <div class="order-box">
+            <div class="order-type-base">
+              BASE
+            </div>
+            <div class="order-type-label">
+              LABEL
+            </div>
+          </div>
+
+        </div>
+        <div class="order-kind mt10">
+          <b-form-select v-model="selected" :options="options"></b-form-select>
+        </div>
+        <div class="order-price mt10">
+          <b-button class="minus"><b-icon icon="plus"></b-icon></b-button>
+          <b-form-input></b-form-input>
+          <b-button class="plus"><b-icon icon="dash"></b-icon></b-button>
+        </div>
+        <div class="order-qty mt10">
+          <b-button class="minus"><b-icon icon="plus"></b-icon></b-button>
+          <b-form-input></b-form-input>
+          <b-button class="plus"><b-icon icon="dash"></b-icon></b-button>
+        </div>
+        <div class="order-percent mt10">
+          <div class="quater">
+          <b-button></b-button>
+          25%
+          </div>
+          <div class="half">
+          <b-button></b-button>
+          50%
+          </div>
+          <div class="half-quarter">
+          <b-button></b-button>
+          75%
+          </div>
+          <div class="all">
+          <b-button></b-button>
+          100%
+          </div>
+        </div>
+        <div class="order-total mt10">
+          {{"합계"}}
+        </div>
+        <div class="order-available mt10">
+          <p>사용 가능</p>
+          <p>{{"3838.051342342"}} {{"USDT"}}</p>
+        </div>
+        <div class="order-button mt10">
+          <b-button class="orderBtn btn-block">{{"XRP 매수"}}</b-button>
+        </div>
+        <!-- <b-card no-body>
           <b-tabs pills card>
             <b-tab title="매수" active>
               <b-card-text>
@@ -46,10 +98,10 @@
             </b-tab>
             <b-tab title="매도"><b-card-text>Tab contents 2</b-card-text></b-tab>
           </b-tabs>
-        </b-card>
+        </b-card> -->
       </div>
       <!-- <OrderBook :depth="depth" class="order-book" /> -->
-      <div class="order-book">
+      <div class="order-book-container">
         <b-list-group>
           <div class="ORDER" v-for="(item, index) in S" :key="item + index">
             <b-list-group-item button>
@@ -96,6 +148,12 @@ export default class Trade extends Vue {
   public price = 0;
   public qty = 0;
   public perc = 0;
+  public selected = null;
+  public options= [
+    { value: null, text: 'Please select an option' },
+    { value: 'a', text: 'This is First option' },
+    { value: 'b', text: 'Selected Option' },
+  ]
   get totalPrice() {
     return this.qty * this.price
   }
@@ -116,14 +174,86 @@ export default class Trade extends Vue {
 }
 </script>
 <style lang="scss" scoped>
-.order-wrap {
+.trade {
+  background-color: #eee;
+}
+.mt10 {
+  margin-top: 10px;
+}
+p {
+  margin-bottom: 0;
+}
+.order-container {
   display: flex;
+  padding: 15px;
+  margin-top: 15px;
+  border-radius: 20px 20px 0 0;
+  background-color: #fff;
   .order {
-    &-box {
+    &-box-container {
       width: 60%;
+      padding-right: 10px;
     }
-    &-book {
+    &-book-container {
       width: 40%;
+    }
+    &-box-wrap {
+      width: 100%;
+      .order{
+        &-box {
+          display: flex;
+          width: 100%;
+          border: 1px solid aqua;
+        }
+        &-type-base {
+          width: 45%;
+          background: pink;
+          // display: inline-block;
+          height: 30px;
+          position: relative;
+          &:before {
+            content: "";
+            width: 0;
+            height: 30px;
+            right: -12px;
+            position: absolute;
+            border-style: solid;
+            border-color: transparent transparent transparent pink;
+            border-width: 12px 0 12px 12px;
+            line-height: 30px;
+          }
+        }
+        &-type-label {
+          width: 55%;
+          border: 1px solid yellow;
+        }
+      }
+    }
+    &-price {
+      display: flex;
+    }
+    &-qty {
+      display: flex;
+    }
+    &-percent {
+      display: flex;
+    }
+    &-total {
+      height: 40px;
+      line-height: 40px;
+      border-radius: 3px;
+      background-color: #eee;
+    }
+    &-available {
+      display: flex;
+      justify-content: space-between;
+      padding: 5px;
+      font-size: 13px;
+    }
+  }
+  &-button {
+    .btn.btn-secondary.orderBtn {
+      width: 100%;
     }
   }
 }
